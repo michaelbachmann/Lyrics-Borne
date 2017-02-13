df6d0292148715b14ece55b2c9c1680dfe208b51
John Michael Bachmann <jbachman@usc.edu>
2017-02-12 20:24:54 -0800

    Initial Push, all files added, package.json configured

---
+++ README.md
@@ -0,0 +1,5 @@
+#LyricsBorne Frontend
+
+##Installation
+1. Run `npm install`
+2. Start development server with `npm run dev`
---
+++ src/js/pages/Layout.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router";
+
+export default class Layout extends React.Component {
+	render() {
+
+		return (
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<h1>Title!</h1>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
